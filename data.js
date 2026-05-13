c498e5c2ce6271029e560757de2655122ced81e3
maxhousner <111194873+maxhousner@users.noreply.github.com>
2026-05-13 15:27:33 -0700

    Update data.js

--- data.js
+++ data.js
@@ -139,7 +139,7 @@ const MANAGERS = [
     name: "Bennett",
     teamName: {
       the_masters: "BENNETT'S BOYS",
-      pga_championship: "[TEAM NAME]",
+      pga_championship: "If my wife asks this isn't for money",
       us_open: "[TEAM NAME]",
       the_open: "[TEAM NAME]",
     },
